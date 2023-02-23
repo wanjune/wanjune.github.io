fe52e9f3136ce4b3a12efd9b0b819c0fd1c4f769
wanjune <wanjune@icloud.com>
2023-02-23 17:40:34 +0800

    U2302231740

--- static/tvbox/xpath/hd/huanggua.js
+++ static/tvbox/xpath/hd/huanggua.js
@@ -1,6 +1,6 @@
 var rule = {
   title: '黄瓜TV',
-  host: 'https://zzg37.com:33666',
+  host: 'https://zzg38.com:33666',
   homeUrl: '/home/index.html',
   url: '/home/vodlist/38/fyclass-fypage.html',
   headers: { 'User-Agent': 'MOBILE_UA' },
@@ -8,7 +8,7 @@ var rule = {
   quickSearch: 0,
   filterable: 1,
   timeout: 5000,
-  class_name: '国产偷拍&国产自拍&日韩无码&欧美无码&中文字幕&日韩有码&卡通动漫',
+  class_name: '偷拍&自拍&日韩无码&欧美无码&中文字幕&有码&动漫',
   class_url: '1321&1320&1316&1317&1318&1319&1322',
   limit: 8,
   play_parse: true,
