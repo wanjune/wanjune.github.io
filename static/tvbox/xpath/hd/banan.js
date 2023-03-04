f39fd5c1e987dab9a7b080ca3235c8f37fc21939
wanjune <wanjune@icloud.com>
2023-03-04 16:48:09 +0800

    U2303041648

---
+++ static/tvbox/xpath/hd/banan.js
@@ -0,0 +1,15 @@
+var rule = {
+  title:'banan',
+  host:'https://banan.tv',
+  url:'/vodtype/fyclass-fypage.html',
+  homeUrl:'/label/new.html',
+  headers:{'User-Agent':'MOBILE_UA'},
+  timeout:5000,
+  class_name:'探花&大神&素人&国产&高清&FC2',
+  class_url:'57&23&63&25&22&28',
+  limit:5,
+  play_parse:true,
+  lazy:'',
+  一级:'.img-box.cover-md&&a;img&&alt;img&&data-src;.grid_date&&Text;a&&href',
+  二级:'*',
+}
