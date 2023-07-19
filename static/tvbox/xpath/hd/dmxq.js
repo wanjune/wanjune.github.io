4d1599849bf0d09342863d7808541aba7fab3f53
wanjune <wanjune@icloud.com>
2023-07-19 14:05:55 +0800

    2307191405

--- static/tvbox/xpath/hd/dmxq.js
+++ static/tvbox/xpath/hd/dmxq.js
@@ -1,9 +1,8 @@
-// 地址发布页 https://www.dmys.tv/ https://dmdyy.cc/ https://www.dmxq.vip/
 muban.mxpro.二级.desc = '.module-info-item:(3)&&Text;;;.module-info-item-content:eq(1)&&Text;.module-info-item-content:eq(0)&&Text';
 muban.mxpro.二级.img = '.lazyload&&src';
 var rule = {
   title:'大米星球',
-  desc:'https://dmdyy.cc/',
+  desc:'https://dmxq.net https://www.dmdy.vip https://www.dmdyy.cc/',
   模板:'mxpro',
   host:'https://www.dmdy15.cc',
   url:'/vodshow/fyclassfyfilter.html',
